9fd1fdb80b476959191cca2358176a625adae8e6
Oswaldo Rubio <oswaldo.rubio@xing.com>
2023-05-11 15:41:41 +0200

    Initial Commit

---
+++ ReallySimpleApp/ReallySimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReallySimpleAppApp.swift
+//  ReallySimpleApp
+//
+//  Created by Oswaldo Rubio on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ReallySimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
